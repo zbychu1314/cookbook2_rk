3c71a6740b1259e24309dfb64a21bf4032e151bd
rkorzen <korzeniewski@gmail.com>
2023-11-25 14:41:31 +0100

    widoki postow

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+# adres -> funkcja -> szablon
+# posts -> list -> posts/list.html
+
+# posts/<int:id> -> details -> posts/details.html
+
+# GET posts/add -> create -> posts/add.html tu trzeba utworzyc formularz <form method="POST">
+# POST posts/add -> create -> operacj dodania a nastepnie redirect
